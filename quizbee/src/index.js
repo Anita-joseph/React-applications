b52d0efcb8f88c3f42828afffc0e10f865059a69
Anita Joseph <anitarosaire@gmail.com>
2020-11-12 20:59:47 +0100

    quiz initial commit

---
+++ quizbee/src/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class QuizBee extends Component {
+    render() {
+        return ( < div className = 'container' >
+            <
+            div className = 'title' > QuizBee < /div> </div >
+        );
+    }
+}
+
+ReactDOM.render( < QuizBee / > , document.getElementById('root'));
